321d5aa39360271d7e7b5421c3ed984ab50eb7ff
wisemuji <wisemuji@gmail.com>
2018-11-19 01:58:19 +0900

    신청폼 라우터 연결

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
 module.exports = function(app){
-
+  app.post('/join-form', async (req,res)=>{
+    console.log(req.body.id+"");
+  })
 }
