2cef28b7c75a646b30bc4c1e35666bb83ae43d27
wisemuji <wisemuji@gmail.com>
2018-11-19 03:47:03 +0900

    apply 라우터 생성

---
+++ routes/apply/apply.js
@@ -0,0 +1,18 @@
+module.exports = (app, Appliers, rndstring)=>{
+  app.post('/logout', async (req,res)=>{
+
+  })
+
+  .post('/delUser', async (req,res)=>{
+
+  })
+
+  .post('/isAuth',  async (req,res)=>{
+
+  })
+
+  .post('/aa', async(req,res)=>{
+
+  })
+
+};
