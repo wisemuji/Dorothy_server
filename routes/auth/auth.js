f84cd6d082ce4bbbd8c028393c5a36620459938d
wisemuji <wisemuji@gmail.com>
2018-11-19 18:24:48 +0900

    auth 회원가입 로그인 구현

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,18 +1,33 @@
 module.exports = (app, Users, rndstring)=>{
-  app.post('/logout', async (req,res)=>{
-
+  app.post('/signup', async(req,res)=>{
+    console.log('post:signup');
+    var user = new Users(req.body);
+    user.token = rndstring.generate(40);
+    try {
+      var result = await user.save();
+    }catch(e){
+      if(e instanceof user_duplicate) return res.status(409).json({message:"already exist"});
+      if(e instanceof ValidationError) return res.status(400).json({message: e.message});
+      if(e instanceof paramsError) return res.status(400).json({message: e.message});
+    }
+    res.status(200).json(user);
   })
-
+  .post('/signin', async(req,res)=>{
+    console.log('post:signin');
+    var result = await Users.findOne(req.body)
+    if(!result) return res.status(404).json({message : "Not Found"})
+    else return res.status(200).json(result)
+  })
   .post('/delUser', async (req,res)=>{
-
+    console.log('post:delUser');
+    var result = await Users.deleteOne({token : req.body.token})
+      console.log(req.body);
+    if(!result.ok) return res.status(500).json({message : "ERR!"})
+    else return res.status(200).json({message : "success!"})
   })
-
-  .post('/isAuth',  async (req,res)=>{
-
-  })
-
   .post('/aa', async(req,res)=>{
-
+    var result = await Users.find()
+    res.send(result)
   })
 
 };
