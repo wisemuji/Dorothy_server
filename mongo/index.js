03d69d0df434e02dcac6aaa54f865ae5df5ec535
wisemuji <wisemuji@gmail.com>
2019-04-17 08:59:09 +0900

    id and pass 따로 파일 정의

--- mongo/index.js
+++ mongo/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const config = require('../config.js');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://wisemuji:gusanwl0307@ds227865.mlab.com:27865/dorothy', { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://'+config.mongo_id+':'+config.mongo_pass+'@ds227865.mlab.com:27865/dorothy', { useNewUrlParser: true }).then(() => {
 console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
