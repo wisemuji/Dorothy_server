08157e607ca1ea4e894e22f3d1d9b02877f5e652
wisemuji <wisemuji@gmail.com>
2019-05-01 07:46:42 +0900

    cofiguration file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'service': 'gmail',
+    'user': 's2017s25@e-mirim.hs.kr',
+    'pass': 'gusanwl0307^',
+    'mongo_id': 'wisemuji',
+    'mongo_pass': 'gusanwl0307',
+    'accountSid': 'AC1578ca58b181960db2909f202434a199',
+    'authToken': 'ff6ca123868b891a6de2453659de3c93'
+}
