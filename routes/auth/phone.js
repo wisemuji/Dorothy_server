adfe2ca0418a6ededde282559119260938919d66
wisemuji <wisemuji@gmail.com>
2019-04-30 11:00:31 +0900

    주석 추가

--- routes/auth/phone.js
+++ routes/auth/phone.js
@@ -7,17 +7,18 @@ const twilio = require('twilio');
 module.exports = (app, Confirm)=>{
     app.post("/phoneAuth", async (req,res) => {
         const phone = req.body.phone;
-        let phone_token = Math.floor(Math.random() * 9000) + 1000;
+        let phone_token = Math.floor(Math.random() * 9000) + 1000; //랜덤 네자리수 생성
         let client = new twilio(accountSid, authToken);
         let contents = '[도로시] 휴대폰 인증을 위해 인증번호 ['+phone_token+']를 입력해 주세요.'
 
         await client.messages.create({
             body: contents,
-            to: '+82' + phone,  // Text this number
+            to: '+82' + phone,  // Text this number 국가번호 +82
             from: '+12053509816' // From a valid Twilio number
         })
         .then( async (message) => {
             let confirm = await new Confirm({phone: phone, phone_token: phone_token});
+            //찾아서 있으면 업데이트 없으면 추가(upsert)
             await Confirm.findOneAndUpdate({phone: phone}, {phone_token: phone_token}, {upsert: true}, (err)=>{
                 if(err) {
                     res.status(400).json({"message":"error!"}); 
@@ -34,6 +35,7 @@ module.exports = (app, Confirm)=>{
         )
     })
     .post("/phoneAuthCheck", async (req,res) => {
+        //인증번호 확인하면 삭제
         await Confirm.findOneAndRemove({phone:req.body.phone, phone_token: req.body.phone_token}, (err, data)=>{
             if (err){            
                 res.send(err);
