f34eaf4aee16722fc0ec70744b561c75472aa6f5
wisemuji <wisemuji@gmail.com>
2019-04-30 12:09:44 +0900

    appliers view

--- routes/apply/apply.js
+++ routes/apply/apply.js
@@ -3,13 +3,14 @@ module.exports = (app, Clubs, rndstring)=>{
       const token = req.params.token;
       const club = await Clubs.findOne({token: token});
         if(req.session.logined) {
-         res.render('form', {item: club, id: req.session.user_id });
+         res.render('form', {item: club, id: req.session.user_id||req.session.email });
         } else {
-          res.render('form', {item: club, id: false});
+          res.send('<script type="text/javascript">alert("로그인이 필요합니다."); history.back();</script>');
         }
 
   })
   .post('/apply/:token', async (req,res)=>{
+      const email = req.session.email;
       const id = req.body.id;
       const name = req.body.name;
       const pr = req.body.pr;
@@ -18,7 +19,7 @@ module.exports = (app, Clubs, rndstring)=>{
       Clubs.findOne({token: req.params.token}, function(err, rawContent){
           if(err) throw err;
 
-          rawContent.appliers.unshift({ id: id, name: name, pr: pr, reason: reason, token: token });
+          rawContent.appliers.unshift({ email: email, id: id, name: name, pr: pr, reason: reason, token: token });
           rawContent.save(function(err){
               if(err) throw err;
           });
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ module.exports = function(app, Clubs){
      Clubs.find({}).sort({date:-1}).exec(function(err, rawContents){ //오래된 순으로 정렬
        if(err) throw err;
          if(req.session.logined) {
-          res.render('index', {contents: rawContents, id: req.session.user_id });
+          res.render('index', {contents: rawContents, id: req.session.user_id||req.session.email });
          } else {
           res.render('index', {contents: rawContents, id: false});
          }
@@ -17,7 +17,7 @@ module.exports = function(app, Clubs){
     Clubs.find({}).sort({date:0}).exec(function(err, rawContents){
       if(err) throw err;
         if(req.session.logined) {
-         res.render(page + '', {contents: rawContents, id: req.session.user_id });
+         res.render(page + '', {contents: rawContents, id: req.session.user_id||req.session.email });
         } else {
           res.render(page + '', {contents: rawContents, id: false});
         }
