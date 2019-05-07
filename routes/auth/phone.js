d5b2573c173b1298cc750a41bb098a735fc0f446
wisemuji <wisemuji@gmail.com>
2019-05-07 23:13:07 +0900

    phone number array with single value

--- routes/auth/phone.js
+++ routes/auth/phone.js
@@ -38,23 +38,29 @@ module.exports = (app, Confirm)=>{
     .post("/sendPassOrFail", async (req,res) => {
         const club = req.body.club;
         const isPass = req.body.isPass;
-        const phone = req.body.check[0];
-        let contents = '[도로시]동아리 모집 결과 안내\n'+phone+'님이 '+club+'에 최종 \''+isPass+'\'하셨음을 안내해드립니다. 미림 동아리 통합 관리 솔루션 도로시를 이용해주셔서 갑사합니다.'
-        console.log(phone);
-        console.log(contents);
-        await client.messages.create({
-            body: contents,
-            to: '+82' + phone,  // Text this number 국가번호 +82
-            from: '+12053509816' // From a valid Twilio number
-        })
-        .then( async (message) => {
-            res.redirect('/');
-        },
-            (error) => {
-                console.error(error);
+        let elements = req.body.check;
+        if(!Array.isArray(elements)){
+            elements = [];
+            elements.push(req.body.check);
+        }
+        elements.forEach( async (phone)=>{
+            let contents = '[도로시]동아리 모집 결과 안내\n'+phone+'님이 '+club+'에 최종 \''+isPass+'\'하셨음을 안내해드립니다. 미림 동아리 통합 관리 솔루션 도로시를 이용해주셔서 갑사합니다.'
+            console.log(phone);
+            console.log(contents);
+            await client.messages.create({
+                body: contents,
+                to: '+82' + phone,  // Text this number 국가번호 +82
+                from: '+12053509816' // From a valid Twilio number
+            })
+            .then( async (message) => {
                 res.redirect('/');
-            }
-        )
+            },
+                (error) => {
+                    console.error(error);
+                    res.redirect('/');
+                }
+            )
+        });
     })
 
     .post("/phoneAuthCheck", async (req,res) => {
