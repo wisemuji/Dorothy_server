841680a0a81083e4dcdcca15fa2d27357b5938fd
wisemuji <wisemuji@gmail.com>
2019-05-08 13:01:00 +0900

    동아리 관리자가 다른 동아리에 접근 못하게 하기

--- routes/apply/vertify.js
+++ routes/apply/vertify.js
@@ -1,9 +1,9 @@
 module.exports = (app, Clubs)=>{
   app.get('/vertify/:token', async (req,res)=>{
+    if(req.session.club != req.params.token) res.send('<script type="text/javascript">alert("권한이 없습니다."); history.back();</script>');
     const club = await Clubs.findOne({token: req.session.club});
     console.log(req.session.club);
-    if(!club)
-    	res.send('<script type="text/javascript">alert("권한이 없습니다."); history.back();</script>');
+    if(!club )res.send('<script type="text/javascript">alert("권한이 없습니다."); history.back();</script>');
     else {
       res.render('apply_view', {item: club, id: req.session.user_id });
     }
